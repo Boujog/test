d23b9937967a6580dbd09438f8b75495b69ff728
Lizan José <despejl@hotmail.fr>
2021-11-16 11:32:18 +0100

    create random launch

--- javascript/task.js
+++ javascript/task.js
@@ -1,3 +1,21 @@
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+const lancerDe = function() {
+
+  const nombreDecimal =(Math.random() * 6) + 1
+  
+  const nombre = Math.trunc(nombreDecimal)
+
+  return nombre
 }
+
+const deTexte = {
+  '1' : 'Un',
+  '2' : 'Deux',
+  '3' : 'Trois',
+  '4' : 'Quatre',
+  '5' : 'Cinq',
+  '6' : 'Six',
+}
+
+
+const resultat = lancerDe()
+console.log(resultat)
