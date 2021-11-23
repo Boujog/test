504b29a1c09c986f712ece47f7fcb13634d89ac2
Lizan José <despejl@hotmail.fr>
2021-11-23 15:33:51 +0100

    function for win the game

--- javascript/task.js
+++ javascript/task.js
@@ -3,10 +3,11 @@
 var player1 = prompt("what is your name, player 1?");
 var player2 = prompt("what is your name, player 2?");
   //Score
-var score1 = 0;
+var score1 = 100;
 var score2 = 0;
 
-const rollDice = function() {
+  //Dice random
+function rollDice() {
 
   const nombreDecimal =(Math.random() * 6) + 1
   
@@ -14,7 +15,7 @@ const rollDice = function() {
 
   return nombre
 }
-
+ //Dice number return
 const deTexte = {
   '1' : 'Un',
   '2' : 'Deux',
@@ -23,8 +24,20 @@ const deTexte = {
   '5' : 'Cinq',
   '6' : 'Six',
 }
+// Win or Lose
+function theWinner() {
+  if(score1>=100){
+    console.log('${player1} win the game!')
+  }else if(score2>=100){
+    console.log('${player2} win the game!')
+  }else{
+    console.log('Continue the Game')
+  }
+}
+
 
 console.log(player1);
 console.log(player2);
 console.log(score1);
 console.log(score2);
+console.log(theWinner())
