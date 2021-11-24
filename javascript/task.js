d4b8c2ce30db6773ffac8776edb06d0626a69786
Lizan José <despejl@hotmail.fr>
2021-11-24 15:42:20 +0100

    rollDice try function

--- javascript/task.js
+++ javascript/task.js
@@ -1,22 +1,57 @@
 // global Var for Play 
-  //Players
+ /* //Players 
 var player1 = prompt("what is your name, player 1?");
 var player2 = prompt("what is your name, player 2?");
-  //Score
+ */
+/* //Score 
 var score1 = 100;
 var score2 = 0;
-
+ */
   //Dice random
-function rollDice() {
-
-  const nombreDecimal =(Math.random() * 6) + 1
+  /*
+  function rollDice() {
+    var die1 = document.getElementById("die1");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 ;
+    die1.innerHTML = d1;
+    status.innerHTML = "You rolled " + diceTotal+".";
+    if(d1 == 1) {
+        status.innerHTML += "Sorry! You turn is over!!";
+    } else if (d1 === 3) {
+        status.innerHTML += "half life 3 confirmed";
+    }else if (d1 === 6) {
+        status.innerHTML += "Lucky! Take the big Roll"
+    }
   
-  const nombre = Math.trunc(nombreDecimal)
-  
-  return nombre
-  
-  document.getElementById("diceRoll").innerHTML = nombre
-}
+  }
+ */
+  function rollDice() {
+    var die1 = document.getElementById("die1");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 ;
+    die1.innerHTML = d1;
+    status.innerHTML = "You rolled " + diceTotal+".";
+    switch(diceTotal) {
+      case "2":
+        status.innerHTML += "you short!!";
+      break;
+      case "3":
+        status.innerHTML += "not bad";
+      break;
+      case "4":
+        status.innerHTML += "good";
+      break;
+      case "5":
+        status.innerHTML += "good";
+      break;
+      case "6":
+        status.innerHTML += "Lucky!";
+      break;
+      default:
+        status.innerHTML += "Sorry! You turn is over!!";
+  }
  //Dice number return
 const deTexte = {
   '1' : 'Un',
@@ -26,6 +61,14 @@ const deTexte = {
   '5' : 'Cinq',
   '6' : 'Six',
 }
+
+//Tirage de dés images
+function frontDice(){
+  if(number1 == 1){
+    
+  }
+}
+
 // Win or Lose
 function theWinner() {
   if(score1>=100){
@@ -36,10 +79,3 @@ function theWinner() {
     console.log('Continue the Game')
   }
 }
-
-
-console.log(player1);
-console.log(player2);
-console.log(score1);
-console.log(score2);
-console.log(theWinner())
